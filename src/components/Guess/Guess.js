e9dca2a0676a90b2ab808a1259b0d5e7675d8c08
Pooja Shroff <poojashroff91@gmail.com>
2023-09-25 22:53:25 -0700

    minor tweak

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,8 +5,8 @@ function Guess({ result }) {
   return (
     <p className="guess">
       {range(5).map((num) => {
-        const resultClass = (result && result[num]) ? result[num].status : '';
-        return <span key={num} className={`cell ${resultClass}`}>
+        const resultClass = (result && result[num]) ? `cell ${result[num].status}` : 'cell';
+        return <span key={num} className={resultClass}>
         {(result && result[num]) ? result[num].letter : undefined}</span>;
       })}
     </p>
